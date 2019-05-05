e93846d25e8e2c30cee4053e6f355bc5b960bd9d
Felix Mau <me@felix.hamburg>
2019-05-05 15:40:08 +0800

    ✨ :: Added files for CI

---
+++ Scripts/pod-lint.sh
@@ -0,0 +1,7 @@
+#
+# pod-lint.sh
+# Created by Felix Mau (https://felix.hamburg)
+#
+
+#!/bin/bash
+pod lib lint
---
+++ Scripts/test.sh
@@ -0,0 +1,21 @@
+#
+# test.sh
+# Created by Felix Mau (https://felix.hamburg)
+#
+# Based on:
+#  - http://www.mokacoding.com/blog/running-tests-from-the-terminal/
+#  - https://github.com/supermarin/xcpretty#usage
+#  - https://www.objc.io/issues/6-build-tools/travis-ci/
+#
+
+#!/bin/bash
+cd Example/
+
+xcodebuild \
+  -workspace LightweightObservable.xcworkspace \
+  -scheme LightweightObservable-Example \
+  -enableCodeCoverage YES \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone XR,OS=12.0' \
+  ONLY_ACTIVE_ARCH=NO \
+  test | xcpretty && exit ${PIPESTATUS[0]}
