4f46aa4fcefd3f5dc1c46a368952b2041183a7d9
Felix Mau <me@felix.hamburg>
2019-05-18 08:21:54 +0800

    ♻️ :: Moved test cases for equatable-extension into a separate file, to match folder structure from implementation

--- Example/Tests/Observable/Observable+EquatableTestCase.swift
+++ Example/Tests/Observable/Observable+EquatableTestCase.swift
@@ -1,8 +1,8 @@
 //
-//  ObservableTestCase.swift
+//  Observable+EquatableTestCase.swift
 //  LightweightObservable_Tests
 //
-//  Created by Felix Mau on 11/02/19.
+//  Created by Felix Mau on 18/05/19.
 //  Copyright © 2019 Felix Mau. All rights reserved.
 //
 
@@ -10,67 +10,7 @@ import XCTest
 
 @testable import LightweightObservable
 
-class ObservableTestCase: XCTestCase {
-    // MARK: - Private properties
-
-    private var disposeBag: DisposeBag!
-
-    private var oldValue: Int?
-    private var newValue: Int?
-
-    // MARK: - Public methods
-
-    override func setUp() {
-        super.setUp()
-
-        disposeBag = DisposeBag()
-
-        oldValue = nil
-        newValue = nil
-    }
-
-    override func tearDown() {
-        disposeBag = nil
-
-        super.tearDown()
-    }
-
-    // MARK: - Test method `observe(:)`
-
-    func testObservableShouldInformSubscriberWithCorrectValues() {
-        // Given
-        let variable = Variable(0)
-
-        // When
-        variable.asObservable.subscribe { newValue, oldValue in
-            self.newValue = newValue
-            self.oldValue = oldValue
-        }.disposed(by: &disposeBag)
-
-        // Then
-        XCTAssertEqual(newValue, 0)
-        XCTAssertNil(oldValue)
-    }
-
-    func testObservableShouldUpdateSubscriberWithCorrectValues() {
-        // Given
-        let variable = Variable(0)
-
-        variable.asObservable.subscribe { newValue, oldValue in
-            self.newValue = newValue
-            self.oldValue = oldValue
-        }.disposed(by: &disposeBag)
-
-        // When
-        for value in 1 ..< 10 {
-            variable.value = value
-
-            // Then
-            XCTAssertEqual(newValue, value)
-            XCTAssertEqual(oldValue, value - 1)
-        }
-    }
-
+extension ObservableTestCase {
     // MARK: - Test method `subscribe(filter:)`
 
     func testObservableShouldUpdateSubscriberOnlyOnFilterMatched() {
@@ -160,47 +100,6 @@ class ObservableTestCase: XCTestCase {
         }
 
         waitForExpectations(timeout: 0.1, handler: nil)
-    }
-
-    // MARK: - Test deallocated disposable
-
-    func testObservableShouldNotInformSubscriberAfterDeallocatedDisposable() {
-        // Given
-        let variable = Variable(0)
-
-        var disposable: Disposable? = variable.asObservable.subscribe { newValue, oldValue in
-            self.newValue = newValue
-            self.oldValue = oldValue
-        }
-
-        // Workaround to fix warning `Variable 'disposable' was written to, but never read`
-        XCTAssertNotNil(disposable, "Precondition failed - Expected to have a valid instance at this point.")
-
-        // When
-        disposable = nil
-        variable.value = 1
-
-        // Then
-        XCTAssertEqual(newValue, 0, "As we've deallocated our disposable the observer should not be updated.")
-        XCTAssertNil(oldValue)
-    }
-
-    func testObservableShouldNotInformSubscriberAfterDeallocatedDisposeBag() {
-        // Given
-        let variable = Variable(0)
-
-        variable.asObservable.subscribe { newValue, oldValue in
-            self.newValue = newValue
-            self.oldValue = oldValue
-        }.disposed(by: &disposeBag)
-
-        // When
-        disposeBag = nil
-        variable.value = 1
-
-        // Then
-        XCTAssertEqual(newValue, 0, "As we've deallocated our disposable the observer should not be updated.")
-        XCTAssertNil(oldValue)
     }
 }
 
---
+++ Example/Tests/Observable/ObservableTestCase.swift
@@ -0,0 +1,114 @@
+//
+//  ObservableTestCase.swift
+//  LightweightObservable_Tests
+//
+//  Created by Felix Mau on 11/02/19.
+//  Copyright © 2019 Felix Mau. All rights reserved.
+//
+
+import XCTest
+
+@testable import LightweightObservable
+
+class ObservableTestCase: XCTestCase {
+    // MARK: - Private properties
+
+    var disposeBag: DisposeBag!
+
+    var oldValue: Int?
+    var newValue: Int?
+
+    // MARK: - Public methods
+
+    override func setUp() {
+        super.setUp()
+
+        disposeBag = DisposeBag()
+
+        oldValue = nil
+        newValue = nil
+    }
+
+    override func tearDown() {
+        disposeBag = nil
+
+        super.tearDown()
+    }
+
+    // MARK: - Test method `observe(:)`
+
+    func testObservableShouldInformSubscriberWithCorrectValues() {
+        // Given
+        let variable = Variable(0)
+
+        // When
+        variable.asObservable.subscribe { newValue, oldValue in
+            self.newValue = newValue
+            self.oldValue = oldValue
+        }.disposed(by: &disposeBag)
+
+        // Then
+        XCTAssertEqual(newValue, 0)
+        XCTAssertNil(oldValue)
+    }
+
+    func testObservableShouldUpdateSubscriberWithCorrectValues() {
+        // Given
+        let variable = Variable(0)
+
+        variable.asObservable.subscribe { newValue, oldValue in
+            self.newValue = newValue
+            self.oldValue = oldValue
+        }.disposed(by: &disposeBag)
+
+        // When
+        for value in 1 ..< 10 {
+            variable.value = value
+
+            // Then
+            XCTAssertEqual(newValue, value)
+            XCTAssertEqual(oldValue, value - 1)
+        }
+    }
+
+    // MARK: - Test deallocated disposable
+
+    func testObservableShouldNotInformSubscriberAfterDeallocatedDisposable() {
+        // Given
+        let variable = Variable(0)
+
+        var disposable: Disposable? = variable.asObservable.subscribe { newValue, oldValue in
+            self.newValue = newValue
+            self.oldValue = oldValue
+        }
+
+        // Workaround to fix warning `Variable 'disposable' was written to, but never read`
+        XCTAssertNotNil(disposable, "Precondition failed - Expected to have a valid instance at this point.")
+
+        // When
+        disposable = nil
+        variable.value = 1
+
+        // Then
+        XCTAssertEqual(newValue, 0, "As we've deallocated our disposable the observer should not be updated.")
+        XCTAssertNil(oldValue)
+    }
+
+    func testObservableShouldNotInformSubscriberAfterDeallocatedDisposeBag() {
+        // Given
+        let variable = Variable(0)
+
+        variable.asObservable.subscribe { newValue, oldValue in
+            self.newValue = newValue
+            self.oldValue = oldValue
+        }.disposed(by: &disposeBag)
+
+        // When
+        disposeBag = nil
+        variable.value = 1
+
+        // Then
+        XCTAssertEqual(newValue, 0, "As we've deallocated our disposable the observer should not be updated.")
+        XCTAssertNil(oldValue)
+    }
+}
