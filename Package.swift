2d74cb1b05f094f9329d75edf542f1a22aac6952
Felix Mau <me@felix.hamburg>
2019-09-22 15:38:00 +0200

    ✨ :: add support for swift package manager

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LightweightObservable",
+    products: [
+        .library(
+            name: "LightweightObservable",
+            targets: ["LightweightObservable"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "LightweightObservable",
+            path: "LightweightObservable/Classes/"
+        ),
+        .testTarget(
+            name: "LightweightObservableTests",
+            dependencies: ["LightweightObservable"],
+            path: "Example/Tests/"
+        )
+    ]
+)
