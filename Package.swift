ee672c40c91e829d68032d2e52e8c0138a44009c
Felix Mau <felix.mau@moia.io>
2022-02-06 10:29:09 +0100

    ♻️ :: Adapt SPM

--- Package.swift
+++ Package.swift
@@ -5,13 +5,15 @@ import PackageDescription
 
 let package = Package(name: "LightweightObservable",
                       products: [
+                          // The external product of our package is an importable
+                          // library that has the same name as the package itself.
                           .library(name: "LightweightObservable",
                                    targets: ["LightweightObservable"]),
                       ],
                       targets: [
                           .target(name: "LightweightObservable",
                                   path: "LightweightObservable/"),
-                          .testTarget(name: "LightweightObservableTests",
+                          .testTarget(name: "ExampleTests",
                                       dependencies: ["LightweightObservable"],
-                                      path: "Example/Tests/"),
+                                      path: "Example/ExampleTests/"),
                       ])
